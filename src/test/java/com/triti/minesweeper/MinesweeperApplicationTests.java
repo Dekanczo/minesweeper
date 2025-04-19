90fbd4f2f01894d498cc658834f9445165d0986e
Denis Galustov <galustov@sfedu.ru>
2025-04-19 17:48:00 +0300

    Инициализация проекта

---
+++ src/test/java/com/triti/minesweeper/MinesweeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triti.minesweeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
